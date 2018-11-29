3898279e7f739ca8572a99843c8eb2e40567d4f8
banrih <banricph@gmail.com>
2018-11-30 08:15:20 +0900

    Add Test to Check User Cannot See Other's Tasks

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 describe 'タスク管理機能', type: :system do
   describe '一般表示機能' do
     before do
-      # ユーザーAを作成
       user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      # 作成者がAであるタスクを作成
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
     end
 
     context 'ユーザーAがログインしている時' do
       before do
-        # ユーザーAでログインする
         visit login_path
         fill_in 'メールアドレス', with: 'a@example.com' 
         fill_in 'パスワード', with: 'password'
@@ -19,8 +16,21 @@ describe 'タスク管理機能', type: :system do
       end
 
       it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面に表示されることを確認
         expect(page).to have_content '最初のタスク'
+      end
+    end
+
+    context 'ユーザーBがログインしている時' do
+      before do
+        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+        visit login_path
+        fill_in 'メールアドレス', with: 'b@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        expect(page).to have_no_content '最初のタスク'
       end
     end
   end
